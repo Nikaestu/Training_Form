ce5b9f39f8ac9cb9c338a6a4a39a3c401494d0b6
clone <lehir-dylan@hotmail.com>
2024-08-13 10:19:20 +0200

    Initial Commit

---
+++ 100DaysSwiftUI/_00DaysSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DaysSwiftUIApp.swift
+//  100DaysSwiftUI
+//
+//  Created by Dylan Le Hir on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct _00DaysSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
